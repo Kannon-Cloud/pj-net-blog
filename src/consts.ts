a7cd82bfb201fc827dcdca93dc6e53ca3f91865e
Kannon-Cloud <PaytenJ@pjnet.tech>
2025-07-01 14:05:20 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "PJ Net";
+export const SITE_DESCRIPTION = "Sharing my expertise and love for technology in the field of networking";
